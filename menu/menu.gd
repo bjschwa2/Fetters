88ccb409859a7d62d22552646dcf9b52ab7dbd26
bradley schwarz <bjschwa2@gmail.com>
2017-10-08 15:16:51 -0400

    created level page transitions working

--- menu/menu.gd
+++ menu/menu.gd
@@ -7,8 +7,9 @@ var current_screen = "start"
 var old_size = Vector2()
 
 onready var levels = get_node("Levels")
-onready var options = get_node("Options")
-onready var tween = get_node("homeTween")
+#onready var options = get_node("options")
+#onready var credits = get_node("credits")
+onready var tween = get_node("Tween")
 
 func _ready():
 	# Main menu buttons
@@ -32,6 +33,10 @@ func _ready():
 			minimum_size.x = max(minimum_size.x, child_minimum_size.x)
 			minimum_size.y = max(minimum_size.y, child_minimum_size.y)
 	screenManager.set_minimum_size(minimum_size)
+	
+	# Splash fadeout
+	#if ScenesManager.is_first_load:
+	#	get_node("animation_player").play("SplashFade")
 	
 	# Prepare to move thing when the aspect ratio changes
 	connect("resized", self, "reposition_screens")
@@ -57,25 +62,26 @@ func reposition_screens():
 	
 	#options.set_margin(MARGIN_LEFT, -size.x)
 	#options.set_margin(MARGIN_RIGHT, size.x)
-
+	
+	#credits.set_margin(MARGIN_TOP, size.y)
+	#credits.set_margin(MARGIN_BOTTOM, -size.y)
 	
 	var initial_size = screenManager.get_original_size()
 	var scale_vector = size / initial_size
 	var scale = max(scale_vector.x, scale_vector.y)
 	
 	if  scale > 1:
-		get_node("backgroundLayer").set_scale(Vector2(scale,scale))
-		
+		get_node("background_layer").set_scale(Vector2(scale,scale))
+		#get_node("initial_splash").set_offset((size - initial_size) * Vector2(0.5, 0))
 		if scale_vector.y / scale_vector.x < 1:
-			get_node("backgroundLayer").set_offset(Vector2(0, (1 - scale_vector.x / scale_vector.y) * initial_size.y))
+			get_node("background_layer").set_offset(Vector2(0, (1 - scale_vector.x / scale_vector.y) * initial_size.y))
 		else:
-			get_node("backgroundLayer").set_offset(Vector2(0, 0))
+			get_node("background_layer").set_offset(Vector2(0, 0))
 	tween.remove_all()
 	go_to_target(current_screen, false)
 
 func go_to_target(screen = "start", animate = true):
 	current_screen = screen
-	
 	var target_coordinates = Vector2(0, 0) # By default, use the 0, 0 coordinates
 	if has_node(screen):
 		target_coordinates = get_node(screen).get_pos() # If possible, use the coordinates of the target
